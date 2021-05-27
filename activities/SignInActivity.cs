6b97b6d0f6b755d309329105bc91316703e427d9
Whiz75 <75149500+Whiz75@users.noreply.github.com>
2021-05-27 12:17:10 +0200

    Add project files.

---
+++ activities/SignInActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Democrat.activities
+{
+    [Activity(Label = "Activity1")]
+    public class SignInActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.layout1);
+        }
+    }
+}
